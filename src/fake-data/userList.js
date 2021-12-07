13d009420d2e32b2162f5ace9d9996f0cdc32f3b
Ontorok <nasir.amath07@gmail.com>
2021-12-08 01:07:14 +0600

    user delte

---
+++ src/fake-data/userList.js
@@ -0,0 +1,30 @@
+/**
+ * Title: Dummy data for user list
+ * Description: dummy data for user list
+ * Author: Nasir Ahmed
+ * Date: 08-December-2021
+ * Modified: 08-December-2021
+ * */
+
+const rows = [
+  {
+    id: 1,
+    username: 'nasir',
+    avatar:
+      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+    email: 'nasir@mailcom',
+    transaction: '$ 789',
+    status: 'active'
+  },
+  {
+    id: 2,
+    username: 'rima',
+    avatar:
+      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
+    email: 'rima@mail.com',
+    transaction: '$ 741',
+    status: 'inactive'
+  }
+];
+
+export default rows;
--- src/parts/userList/UserList.jsx
+++ src/parts/userList/UserList.jsx
@@ -1,84 +1,93 @@
+/**
+ * Title: User List
+ * Description: User List
+ * Author: Nasir Ahmed
+ * Date: 07-December-2021
+ * Modified: 07-December-2021
+ * */
+
+import { DeleteOutline, EditOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import rows from '../../fake-data/userList';
 import classes from './UserList.module.css';
 
-const columns = [
-  {
-    field: 'username',
-    headerName: 'User name',
-    width: 150,
-    renderCell: (params) => (
-      <div className={classes.userListUser}>
-        <img className={classes.userListImage} src={params.row.avatar} alt={params.row.username} />
-        {params.row.username}
-      </div>
-    )
-  },
-  // { field: 'avatar', headerName: 'Avatar', width: 130 },
-  {
-    field: 'email',
-    headerName: 'Email',
-    width: 150
-  },
-  {
-    field: 'transaction',
-    headerName: 'Transaction',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 150,
-    center: true
-  },
-  {
-    field: 'status',
-    headerName: 'Status',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 130
-  },
-  {
-    field: 'action',
-    headerName: 'Action',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 130,
-    renderCell: (param) => (
-      <button type="button" onClick={() => console.log(param.row.username)}>
-        add
-      </button>
-    )
-  }
-];
-
-const rows = [
-  {
-    id: 1,
-    username: 'nasir',
-    avatar:
-      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-    email: 'nasir@mailcom',
-    transaction: '$ 789',
-    status: 'active'
-  },
-  {
-    id: 2,
-    username: 'rima',
-    avatar:
-      'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-    email: 'rima@mail.com',
-    transaction: '$ 741',
-    status: 'inactive'
-  }
-];
-
 const UserList = function () {
+  const [state, setState] = useState(rows);
+
+  const handleDelete = (id) => {
+    setState(rows.filter((item) => item.id !== id));
+  };
+
+  const columns = [
+    {
+      field: 'username',
+      headerName: 'User name',
+      width: 200,
+      renderCell: (params) => (
+        <div className={classes.userListUser}>
+          <img
+            className={classes.userListImage}
+            src={params.row.avatar}
+            alt={params.row.username}
+          />
+          {params.row.username}
+        </div>
+      )
+    },
+    {
+      field: 'email',
+      headerName: 'Email',
+      width: 200
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 200,
+      center: true
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 200
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 200,
+      renderCell: (param) => (
+        <>
+          <Link to={`/users/${param.row.id}`}>
+            <IconButton aria-label="edit">
+              <EditOutlined color="success" />
+            </IconButton>
+          </Link>
+
+          <IconButton aria-label="delete" onClick={() => handleDelete(param.row.id)}>
+            <DeleteOutline color="error" />
+          </IconButton>
+        </>
+      )
+    }
+  ];
   return (
     <div className={classes.userList}>
       <DataGrid
-        rows={rows}
+        autoPageSize
+        rows={state}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
+        disableSelectionOnClick
       />
     </div>
   );
